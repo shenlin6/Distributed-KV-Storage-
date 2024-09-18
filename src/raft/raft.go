14e5b2957136dc4bdc89c9d4f8598229d143bc7f
Shone <1457811364@qq.com>
2024-09-18 11:23:13 +0800

    switch state

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -28,6 +28,14 @@ import (
 	"course/labrpc"
 )
 
+type Role string
+
+const (
+	Follower  Role = "Follwer"
+	Candidate Role = "Candidate"
+	Leader    Role = "Leader"
+)
+
 // as each Raft peer becomes aware that successive log entries are
 // committed, the peer should send an ApplyMsg to the service (or
 // tester) on the same server, via the applyCh passed to Make(). set
@@ -60,7 +68,47 @@ type Raft struct {
 	// Your data here (PartA, PartB, PartC).
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
+	role        Role
+	currentTerm int
+	votedFor    int //-1 means vote for none
 
+	electionStart   time.Time
+	electionTimeOut time.Duration // Random
+}
+
+func (rf *Raft) becomeFollowerLocked(term int) {
+	if term < rf.currentTerm {
+		LOG(rf.me, rf.currentTerm, DError, "Can't become Follower,lower term:T%d", term)
+		return
+	}
+	LOG(rf.me, rf.currentTerm, DLog, "%s->Follower,For T%d->T%d", rf.role, rf.currentTerm, term)
+	rf.role = Follower
+	//投票置空
+	if term > rf.currentTerm {
+		rf.votedFor = -1
+	}
+	rf.currentTerm = term
+}
+
+func (rf *Raft) becomeCandidate() {
+	if rf.role == Leader {
+		LOG(rf.me, rf.currentTerm, DError, "Leader can't become a candidate")
+		return
+	}
+
+	LOG(rf.me, rf.currentTerm, DVote, "%s->Candidate,For T%d", rf.role, rf.currentTerm+1) //提前打日志，防止覆盖
+	rf.currentTerm++
+	rf.role = Candidate
+	rf.votedFor = rf.me
+}
+
+func (rf *Raft) becomeLeader() {
+	if rf.role != Candidate {
+		LOG(rf.me, rf.currentTerm, DError, "Only Candidate can become a Leader")
+		return
+	}
+	LOG(rf.me, rf.currentTerm, DLeader, "Become a Leader in T%d", rf.currentTerm)
+	rf.role = Leader
 }
 
 // return currentTerm and whether this server
@@ -240,6 +288,9 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.me = me
 
 	// Your initialization code here (PartA, PartB, PartC).
+	rf.role = Follower
+	rf.currentTerm = 0
+	rf.votedFor = -1
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
