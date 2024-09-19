07467ad12e041a1583441d892d575f822c44f124
Shone <1457811364@qq.com>
2024-09-19 15:06:29 +0800

    实现心跳逻辑

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -31,6 +31,7 @@ import (
 const (
 	electionTimeOutMin time.Duration = 250 * time.Millisecond
 	electionTimeOutMax time.Duration = 400 * time.Millisecond
+	replicateInterval  time.Duration = 200 * time.Millisecond
 )
 
 // resetElectionLocked 重置选举时钟（加锁才能用）
@@ -308,6 +309,96 @@ func (rf *Raft) killed() bool {
 // 需要及时终止回调函数、请求返回的处理，否则状态机会产生问题
 func (rf *Raft) contextLostLocked(role Role, term int) bool {
 	return !(rf.currentTerm == term && rf.role == role)
+}
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId int
+}
+
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
+
+// AppendEntries 接收方接受心跳
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	//对齐 term
+	if args.Term < rf.currentTerm {
+		LOG(rf.me, rf.currentTerm, DLog2, "<- S%d,Reject log,Higher term,T%d<T%d", args.LeaderId, args.Term, rf.currentTerm)
+		return
+	}
+	if args.Term >= rf.currentTerm {
+		rf.becomeFollowerLocked(args.Term)
+	}
+
+	//重置时钟
+	rf.resetElectionLocked()
+}
+
+// sendAppendEntries 发送方发送心跳
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+	return ok
+}
+
+// startReplication 对所有 peer 发送 RPC
+func (rf *Raft) startReplication(term int) bool {
+	replicateToPeer := func(peer int, args *AppendEntriesArgs) {
+		reply := &AppendEntriesReply{}
+		ok := rf.sendAppendEntries(peer, args, reply)
+
+		rf.mu.Lock()
+		defer rf.mu.Unlock()
+		if !ok {
+			LOG(rf.me, rf.currentTerm, DLog, "->S%d Lost or crashed", peer)
+		}
+
+		//对齐 term(发现对方 term 更高就变成他的 Follower)
+		if reply.Term > rf.currentTerm {
+			rf.becomeFollowerLocked(reply.Term)
+			return
+		}
+
+	}
+
+	// rf.currentTerm 可能被并发修改，需要加锁
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	// 如果上下文丢失那么则不再发送心跳
+	if rf.contextLostLocked(Leader, term) {
+		LOG(rf.me, rf.currentTerm, DLog, "Lost Leader[T%d] to %s[T%d]", term, rf.role, rf.currentTerm)
+		return false
+	}
+
+	for peer := 0; peer < len(rf.peers); peer++ {
+		if peer == rf.me {
+			continue
+		}
+
+		args := &AppendEntriesArgs{
+			Term:     rf.currentTerm,
+			LeaderId: rf.me,
+		}
+		go replicateToPeer(peer, args)
+	}
+}
+
+// replicationTicker 心跳（日志同步）：只能在传入的 term 内才能进行日志同步
+func (rf *Raft) replicationTicker(term int) {
+	for !rf.killed() {
+		ok := rf.startReplication(term)
+		if !ok {
+			break
+		}
+
+		// sleep 一个比 electionTimeOutMin 更小的时间，防止有异心的 peer 发起选举
+		time.Sleep(replicateInterval)
+	}
 }
 
 // startElection 发起选举，只对当前term负责
