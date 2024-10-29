5e20d5c20bde6de011db553c306a73caefc188b4
Shone <1457811364@qq.com>
2024-10-29 19:29:19 +0800

    kvraft-client

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,3 @@
-module course
+module "6.824"
 
 go 1.15
--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -4,10 +4,10 @@ import "6.824/labrpc"
 import "crypto/rand"
 import "math/big"
 
-
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leaderId int // 记录 leaderId，避免重复轮询查找leaderId
 }
 
 func nrand() int64 {
@@ -21,10 +21,12 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+
+	ck.leaderId = 0
 	return ck
 }
 
-//
+// Get
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -35,13 +37,30 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
+
+	args := &GetArgs{
+		Key: key,
+	}
+
+	var reply GetReply
+
+	for {
+		ok := ck.servers[ck.leaderId].Call("KVServer.Get", &args, &reply)
+		if !ok || reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			//请求失败，挑选下一个节点
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
+		}
+		// 调用成功，返回 Value
+		return reply.Value
+	}
+
 	return ""
 }
 
+// PutAppend
 //
 // shared by Put and Append.
 //
@@ -51,9 +70,26 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := &PutAppendArgs{
+		Key:   key,
+		Value: value,
+		Op:    op,
+	}
+
+	var reply PutAppendReply
+
+	for {
+		ok := ck.servers[ck.leaderId].Call("KVServer.PutAppend", &args, &reply)
+		if !ok || reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			//请求失败，挑选下一个节点
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
+		}
+		//调用成功，返回
+		return
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
