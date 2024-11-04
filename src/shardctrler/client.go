9ac3509a494f8b2f68b93085f567487160ecffd9
Shone <1457811364@qq.com>
2024-11-04 17:12:16 +0800

    shard controller clientend

--- src/shardctrler/client.go
+++ src/shardctrler/client.go
@@ -5,13 +5,15 @@ package shardctrler
 //
 
 import "6.824/labrpc"
-import "time"
 import "crypto/rand"
 import "math/big"
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
+	leaderId int
+	clientId int64
+	seqId    int64
 }
 
 func nrand() int64 {
@@ -25,6 +27,10 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// Your code here.
+
+	ck.leaderId = 0
+	ck.clientId = nrand()
+	ck.seqId = 0
 	return ck
 }
 
@@ -34,68 +40,77 @@ func (ck *Clerk) Query(num int) Config {
 	args.Num = num
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
-			var reply QueryReply
-			ok := srv.Call("ShardCtrler.Query", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return reply.Config
-			}
+
+		var reply QueryReply
+		ok := ck.servers[ck.leaderId].Call("ShardCtrler.Query", args, &reply)
+		if !ok && reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			// 换一个节点重试请求
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+		return reply.Config
 	}
 }
 
+// Join 添加新的 group
 func (ck *Clerk) Join(servers map[int][]string) {
-	args := &JoinArgs{}
+	args := &JoinArgs{clientId: ck.clientId, seqId: ck.seqId}
 	// Your code here.
 	args.Servers = servers
 
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
-			var reply JoinReply
-			ok := srv.Call("ShardCtrler.Join", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
+
+		var reply JoinReply
+		ok := ck.servers[ck.leaderId].Call("ShardCtrler.Join", args, &reply)
+		if !ok && reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			// 换一个节点重试请求
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+		ck.seqId++
+		return
 	}
 }
 
+// Leave 这些 Group 退出了分布式集群
 func (ck *Clerk) Leave(gids []int) {
-	args := &LeaveArgs{}
+	args := &LeaveArgs{clientId: ck.clientId, seqId: ck.seqId}
 	// Your code here.
 	args.GIDs = gids
 
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
-			var reply LeaveReply
-			ok := srv.Call("ShardCtrler.Leave", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
+
+		var reply LeaveReply
+		ok := ck.servers[ck.leaderId].Call("ShardCtrler.Leave", args, &reply)
+		if !ok && reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			// 换一个节点重试请求
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+		ck.seqId++
+		return
 	}
 }
 
+// Move 将 shard 移动到新的 groupId 中
 func (ck *Clerk) Move(shard int, gid int) {
-	args := &MoveArgs{}
+	args := &MoveArgs{clientId: ck.clientId, seqId: ck.seqId}
 	// Your code here.
 	args.Shard = shard
 	args.GID = gid
 
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
-			var reply MoveReply
-			ok := srv.Call("ShardCtrler.Move", args, &reply)
-			if ok && reply.WrongLeader == false {
-				return
-			}
+		var reply MoveReply
+		ok := ck.servers[ck.leaderId].Call("ShardCtrler.Move", args, &reply)
+		if !ok && reply.Err == ErrWrongLeader || reply.Err == ErrTimeout {
+			// 换一个节点重试请求
+			ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+			continue
 		}
-		time.Sleep(100 * time.Millisecond)
+		ck.seqId++
+		return
 	}
 }
